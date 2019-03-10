07c805c25c91b44caf5ff826969dfa53f9b6f2c0
a6002917 <rajeshkumar.ganesan@bestbuy.com>
2019-03-10 13:45:58 -0500

    UI base code commit

---
+++ src/components/Footer.js
@@ -0,0 +1,81 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import { List, ListItem, withStyles } from "@material-ui/core";
+
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+
+import footerStyle from "../assets/styles/footerStyle.jsx";
+
+class Footer extends React.Component{
+
+    render(){
+    const { classes, whiteFont } = this.props;
+    const footerClasses = classNames({
+        [classes.footer]: true,
+        [classes.footerWhiteFont]: whiteFont
+    });
+    const aClasses = classNames({
+        [classes.a]: true,
+        [classes.footerWhiteFont]: whiteFont
+    });
+    return(
+    <footer className={footerClasses}>
+    <div className={classes.container}>
+      <div className={classes.left}>
+        <List className={classes.list}>
+          <ListItem className={classes.inlineBlock}>
+            <a
+              href="https://www.linkedin.com/in/rajeshkumar-g/"
+              className={classes.block}
+              target="_blank"
+            >
+              Dev Team
+            </a>
+          </ListItem>
+          <ListItem className={classes.inlineBlock}>
+            <a
+              href="https://www.linkedin.com/in/rajeshkumar-g/"
+              className={classes.block}
+              target="_blank"
+            >
+              About us
+            </a>
+          </ListItem>
+          <ListItem className={classes.inlineBlock}>
+            <a
+              href="https://www.linkedin.com/in/rajeshkumar-g/"
+              className={classes.block}
+              target="_blank"
+            >
+              Blog
+            </a>
+          </ListItem>
+        </List>
+      </div>
+      <div className={classes.right}>
+        &copy; {1900 + new Date().getYear()} , made with{" "}
+        <Favorite className={classes.icon} /> by{" "}
+        <a
+          href="https://www.linkedin.com/in/rajeshkumar-g/"
+          className={aClasses}
+          target="_blank"
+        >
+          Creative App
+        </a>{" "}
+        for a better web.
+      </div>
+    </div>
+  </footer>
+    );}
+}
+
+Footer.propTypes = {
+    classes: PropTypes.object.isRequired,
+    whiteFont: PropTypes.bool
+  };
+  
+export default withStyles(footerStyle) (Footer);
